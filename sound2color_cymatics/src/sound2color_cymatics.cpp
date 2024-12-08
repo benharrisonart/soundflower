e86dc999e4bd25c1f73f47e282a3c929ea707f7b
Ben Harrison <swharr@gmail.com>
2024-12-08 01:19:36 -0700

    using A2 now not sure of amplitude value target

--- sound2color_cymatics/src/sound2color_cymatics.cpp
+++ sound2color_cymatics/src/sound2color_cymatics.cpp
@@ -19,8 +19,8 @@ const int PIXELPIN = D2; // for sound2color
 int PIXELDELAY = 40;
 Adafruit_NeoPixel pixel(PIXELCOUNT, SPI1, WS2812B);
 
-const int N = 512;
-const int timeStep = 500;
+const int N = 512; // 512
+const int timeStep = 500; //500
 
 unsigned int currentTime, lastTime;
 int i;
@@ -42,6 +42,9 @@ int rndFreq;
 int bri = 20;
 int vColor;
 int startFreq;
+int newNote;
+int freqNow; // set to freqAtMax in BR function calculateDFT to use setting color
+float ampNow; // set by maxAmplitude in BR function calculateDFT to decide if pixels light based on volume
 
 //BUTTON
 const int BTNPIN = A5;
@@ -58,7 +61,7 @@ void setup() {
     Serial.begin(9600);
     waitFor(Serial.isConnected,10000);
     //pinMode(PIXELPIN,OUTPUT); // i used D2 originally 
-    //pinMode(A2,INPUT);
+    pinMode(A2,INPUT);
   // PIXELS
     pixel.begin();
     // rnd_r = random(255);
@@ -86,22 +89,194 @@ void loop() {
 // RATIO MATH TO CONVERT FREQ INTO COLOR
 // 255/220 = 1.159
 // 220/255 = 0.863
+
 /////////////////////////////////////////////
 // ANIMATION COLOR CYCLE
-for (j=minFreq; j<= maxFreq; j++){ 
-  pixel.setPixelColor(0,Wheel(j*1.159)); // 1.159 is 225/220 to convert freq range 440-220 into color range 0-255
-  pixel.show();
-  Serial.printf("J: %i\n",j);
-  delay(100);
 
- if(btnSwitch.isClicked()) {
-            startFreq = random(220,440);
-            j=startFreq;
-            Serial.printf("startFreq: %i\n",startFreq);
-        }
-}
+// for (j=minFreq; j<= maxFreq; j++){ 
+//   pixel.setPixelColor(0,Wheel(j*1.159)); // 1.159 is 225/220 to convert freq range 440-220 into color range 0-255
+//   pixel.show();
+//   Serial.printf("J: %i\n",j);
+//   delay(100);
+
+//  if(btnSwitch.isClicked()) {
+//             startFreq = random(220,440);
+//             j=startFreq;
+//             Serial.printf("startFreq: %i\n",startFreq);
+//         }
+// }
+
 // END ANIMATION COLOR CYCLE
 /////////////////////////////////////////////
+//digitalWrite(D2,HIGH);
+ 
+
+
+    if (ampNow < 9600){
+          Serial.printf("input signal low\n");
+          pixel.clear();pixel.show();
+    }else{
+        while (freqNow > 440){
+          freqNow = freqNow/2;
+        }
+        while (freqNow < 220 && freqNow > 110){
+          freqNow = freqNow*2;
+        }
+        if (freqNow < 110){
+          freqNow = 0;
+        }
+        Serial.printf("freqNow: %i\n", freqNow);   
+        if (freqNow > 1){
+          pixel.setPixelColor(0,Wheel(freqNow*1.159));
+          pixel.show();
+        } else {
+        pixel.clear();pixel.show();
+        }
+      }
+
+
+
+
+
+  i = 0;
+  while(i<N) {
+    currentTime = micros();
+    if(currentTime-lastTime > timeStep) {
+        lastTime = currentTime;
+        t = currentTime / 1000000.0;
+        timeClock[i] = t;
+        signal[i] = analogRead(A2);
+        //signal[i] = analogRead(A1); // was this first
+        i++;
+    }
+  }
+
+
+  calculateDFT(signal, N, timeStep);
+  //digitalWrite(D2,LOW);
+
+}
+
+// Function to calculate the DFT
+void calculateDFT(float *xn, int len, int usec, bool removeBias) {
+    float Xmag;
+    float Xpha;
+    float freq;
+    float Xr, Xi;
+    int N = len;
+    int k, n;
+    float maxAmplitude;
+    int freqAtMax;
+    float bias;
+
+    bias = 0;
+    if (removeBias) {
+      for(k =0; k<len; k++) {
+        bias = bias + xn[k];
+      }      
+    }
+    bias = bias / len;
+
+    maxAmplitude = 0;
+    for (k = 0; k < len/2; k++) {
+        Xr = 0;
+        Xi = 0;
+        for (n = 0; n < len; n++) {
+            Xr = (Xr + (xn[n]-bias) * cos(2 * M_PI * k * n / N));
+            Xi = (Xi - (xn[n]-bias) * sin(2 * M_PI * k * n / N + (M_PI/2)));
+        }
+        freq = (k / float(len)) * (1000000/usec);
+        Xmag = sqrt(pow(Xr,2)+pow(Xi,2));
+        Xpha = atan2(Xi,Xr)*(360/(2*M_PI));
+        //Serial.printf("%0.2f, %0.2f, %0.2f\n",freq, Xmag, Xpha);
+
+        if(Xmag > maxAmplitude) {
+          maxAmplitude = Xmag;
+          freqAtMax = freq;
+        }  
+        freqNow = freqAtMax; // to use when setting color
+        ampNow = maxAmplitude; // if loud enough then light pixels
+    }
+    Serial.printf("\n\nTime: %i - Max amplitude of %0.2f at frequency %i\n",millis(),maxAmplitude,freqAtMax);
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colors are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return pixel.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return pixel.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return pixel.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+
+// publish a hex color to an adafruit mqtt feed
+// colorFeed.publish(String("#")+String(colorValue,HEX));
+
+
+
+
+  
+  // pixel.setPixelColor(0,rndColor);
+  // pixel.setBrightness(bri);
+  // pixel.show();
+  
+ // digitalWrite(PIXELPIN,HIGH);
+  // i = 0;
+  // while(i<N) {
+  //   currentTime = micros();
+  //   if(currentTime-lastTime > timeStep) {
+  //       lastTime = currentTime;
+  //       t = currentTime / 1000000.0;
+  //       timeClock[i] = t;
+  //       signal[i] = analogRead(A2);
+  //       i++;
+  //   }
+  // }
+
+  //vColor = Wheel(rndColor);
+  //Serial.printf("WheelColor: %i\n",vColor);
+  //calculateDFT(signal, N, timeStep);
+ // digitalWrite(PIXELPIN,LOW);
+
+        //rndColor = random(255);
+        //vColor = Wheel(rndColor);
+        // pixel.setPixelColor(0,j,j,j);
+        // pixel.setPixelColor(0,vColor);
+        // pixel.show();
+        // Serial.printf("WheelColor: %i\n",vColor);
+        // delay(500);
+
+
+
+
+
+
+        // COLOR PORTMANTEAU W/ BUTTON
+
+// for (j=minFreq; j<= maxFreq; j++){ 
+// if(btnSwitch.isClicked()) {
+//     newNote = random(220,440);
+//     if (newNote > ){
+
+//     }
+//             j=startFreq;
+//             Serial.printf("startFreq: %i\n",startFreq);
+//         }
+// }
+
+// COLOR PORTMANTEAU W/ BUTTON
+/////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////
+
 
 
 
@@ -155,149 +330,10 @@ for (j=minFreq; j<= maxFreq; j++){
 
 
 
-
-  // pixel.setPixelColor(0,Wheel(0));
-  // pixel.show();
-  // delay(4000);
-  // pixel.setPixelColor(0,Wheel(9361));
-  // pixel.show();
-  // delay(4000);
-  //   pixel.setPixelColor(0,Wheel(18722));
-  // pixel.show();
-  // delay(4000);
-  //   pixel.setPixelColor(0,Wheel(28083));
-  // pixel.show();
-  // delay(4000);
-  //   pixel.setPixelColor(0,Wheel(37444));
-  // pixel.show();
-  // delay(4000);
-  // pixel.setPixelColor(0,Wheel(46805));
-  // pixel.show();
-  // delay(4000);
-  //   pixel.setPixelColor(0,Wheel(56166));
-  // pixel.show();
-  // delay(4000);
-  //   pixel.setPixelColor(0,Wheel(65527));
-  // pixel.show();
-  // delay(4000);
-
-        
-//         //pixel.clear(); //clear requires 'show' to display as 'off'
-//         //pixel.setBrightness(bri);
-//         vColor = Wheel(rndColor);
-//         // pixel.setPixelColor(0,j,j,j);
-//         pixel.setPixelColor(0,vColor,vColor,0);
-//         pixel.show();
-//         Serial.printf("WheelColor: %i\n",vColor);
-//         }
     
     
     
-}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-// Function to calculate the DFT
-void calculateDFT(float *xn, int len, int usec, bool removeBias) {
-    float Xmag;
-    float Xpha;
-    float freq;
-    float Xr, Xi;
-    int N = len;
-    int k, n;
-    float maxAmplitude;
-    int freqAtMax;
-    float bias;
-
-    bias = 0;
-    if (removeBias) {
-      for(k =0; k<len; k++) {
-        bias = bias + xn[k];
-      }      
-    }
-    bias = bias / len;
-
-    maxAmplitude = 0;
-    for (k = 0; k < len/2; k++) {
-        Xr = 0;
-        Xi = 0;
-        for (n = 0; n < len; n++) {
-            Xr = (Xr + (xn[n]-bias) * cos(2 * M_PI * k * n / N));
-            Xi = (Xi - (xn[n]-bias) * sin(2 * M_PI * k * n / N + (M_PI/2)));
-        }
-        freq = (k / float(len)) * (1000000/usec);
-        Xmag = sqrt(pow(Xr,2)+pow(Xi,2));
-        Xpha = atan2(Xi,Xr)*(360/(2*M_PI));
-        //Serial.printf("%0.2f, %0.2f, %0.2f\n",freq, Xmag, Xpha);
-
-        if(Xmag > maxAmplitude) {
-          maxAmplitude = Xmag;
-          freqAtMax = freq;
-        }
-
-        
-    }
-    Serial.printf("\n\nTime: %i - Max amplitude of %0.2f at frequency %i\n",millis(),maxAmplitude,freqAtMax);
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colors are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-   return pixel.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return pixel.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return pixel.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-}
-
-// publish a hex color to an adafruit mqtt feed
-// colorFeed.publish(String("#")+String(colorValue,HEX));
-
-
-
-
-  
-  // pixel.setPixelColor(0,rndColor);
-  // pixel.setBrightness(bri);
-  // pixel.show();
-  
- // digitalWrite(PIXELPIN,HIGH);
-  // i = 0;
-  // while(i<N) {
-  //   currentTime = micros();
-  //   if(currentTime-lastTime > timeStep) {
-  //       lastTime = currentTime;
-  //       t = currentTime / 1000000.0;
-  //       timeClock[i] = t;
-  //       signal[i] = analogRead(A2);
-  //       i++;
-  //   }
-  // }
-
-  //vColor = Wheel(rndColor);
-  //Serial.printf("WheelColor: %i\n",vColor);
-  //calculateDFT(signal, N, timeStep);
- // digitalWrite(PIXELPIN,LOW);
-
-        //rndColor = random(255);
-        //vColor = Wheel(rndColor);
-        // pixel.setPixelColor(0,j,j,j);
-        // pixel.setPixelColor(0,vColor);
-        // pixel.show();
-        // Serial.printf("WheelColor: %i\n",vColor);
-        // delay(500);
